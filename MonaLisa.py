39c26b63facb4925cd0232b960bb762ad9f39cad
Kris Kruse <kriskruse@hotmail.com>
2021-03-08 11:22:32 +0100

    Last commit

--- MonaLisa.py
+++ MonaLisa.py
@@ -1,29 +1,46 @@
-import cv2
 import numpy as np
+import skimage.viewer
+import cv2
 from scipy.signal import convolve2d
 
+# Settings
+filename = "MonaLisa.jpg"
+t = 0.3
+w = 0.1
 
+# Kernel creation function
 def kernel(N, w):
     F = np.ones([N, N])
     F = np.pad(F, N, 'constant', constant_values=-w)
     return F
 
-
+# Threshold apply function
 def threshold(img, t):
-    mask = img < t
-    return mask
+    img[img < t] = 0
+    return img
 
-img = cv2.imread("MonaLisa.jpg")
+
+# Main data manipulation
+img = cv2.imread(filename)
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 out = cv2.normalize(gray.astype('float'), None, 0.0, 1.0, cv2.NORM_MINMAX)
-w = 0.1
 
-print(kernel(4, w))
+# Test for kernel size 1 - 10
+for N in range(10):
+    convimg = convolve2d(out, kernel(N, w))
+    convimg = threshold(convimg, t)
+    viewer1 = skimage.viewer.ImageViewer(convimg)
+    viewer1.show()
+    print(N)
 
-convimg = convolve2d(out, kernel(10, w))
 
-cv2.imshow("out", convimg)
-cv2.imshow("thres", threshold(convimg,0.8))
-cv2.imshow("original", out)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+# Check size of arrays
+# print(np.shape(img))
+# print(np.shape(gray))
+
+
+# display the image
+# viewergray = skimage.viewer.ImageViewer(gray)
+# viewergray.show()
+
+
